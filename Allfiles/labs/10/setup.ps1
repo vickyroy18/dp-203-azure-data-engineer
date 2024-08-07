a0719744496fe95c4d24cd6cf1f1b793698bc765
vickyroy18 <31153778+vickyroy18@users.noreply.github.com>
2024-08-07 07:29:14 +0530

    Update setup.ps1

--- Allfiles/labs/10/setup.ps1
+++ Allfiles/labs/10/setup.ps1
@@ -7,17 +7,4 @@ $sqlPassword = "password123$"
 write-host "Creating the $sqlDatabaseName database..."
 sqlcmd -S "$synapseWorkspace.sql.azuresynapse.net" -U $sqlUser -P $sqlPassword -d $sqlDatabaseName -I -i setup.sql
 
-# # Upload files
-# write-host "Uploading files..."
-# $storageAccount = Get-AzStorageAccount -ResourceGroupName $resourceGroupName -Name $dataLakeAccountName
-# $storageContext = $storageAccount.Context
-# Get-ChildItem "./data/*.csv" -File | Foreach-Object {
-#     write-host ""
-#     $file = $_.Name
-#     Write-Host $file
-#     $blobPath = "data/$file"
-#     Set-AzStorageBlobContent -File $_.FullName -Container "files" -Blob $blobPath -Context $storageContext
-# }
-
-
 write-host "Script completed at $(Get-Date)"
